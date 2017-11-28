c9a40a9108d1dccd7db9a7bd7f9aa7bf68d278b4
diana1418 <a20166303@pucp.edu.pe>
2017-11-28 12:08:08 -0500

    add overview

--- js/app.js
+++ js/app.js
@@ -2,5 +2,22 @@
  * Funcionalidad de tu producto
  */
 
+debugger;
+
+window.addEventListener('load', function() {
+  var selectSede = document.getElementById('sede');
+
+  /*
+  //var subSelectSede = null;
+  sede.addEventListener('change', function() {
+    if (option.value) {
+      prom.addEventListener('change', function() {
+        data['option.value'];
+      });
+    }
+  });
+*/
+});
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
