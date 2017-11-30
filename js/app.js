e3893bf225160318b3bf7a87b6f4cd7f494f41e8
MollyGreace <greace.ss@gmail.com>
2017-11-30 05:16:19 -0500

    agrega secciones a mostrarcon data

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
+
+/* Declarando variables */
+
+/* var dataSedes = data; */
+var arequipa = data.AQP; // {2016-2: {…}, 2017-1: {…}}
+var mexico = data.CDMX;
+var lima = data.LIM;
+var santiago = data.SCL;
+
+/* suma de todas las estudiantes de las 2 promos de Arequipa */
+ var allStudentsForSede = (Object.keys(data.AQP['2016-2'].students)).length + (Object.keys(data.AQP['2017-1'].students)).length;
+
+
+add
 /*
  * Funcionalidad de tu producto
  */
 
-debugger;
 
-window.addEventListener('load', function() {
+/* window.addEventListener('load', function() {
+
+
+  
   var selectSede = document.getElementById('sede');
 
   /*
@@ -16,8 +32,6 @@ window.addEventListener('load', function() {
       });
     }
   });
-*/
-});
+ }) */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+
