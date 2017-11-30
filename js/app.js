c70796303912c5d725b99be71f6170d092beb43b
MollyGreace <greace.ss@gmail.com>
2017-11-30 09:18:15 -0500

    añade estilos a contenedores de life y tech skills

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 
 /* Declarando variables */
 
+var containerTotalStudents = document.getElementsByClassName('container-total');
+var subSelectPromo = null;
+
 /* var dataSedes = data; */
 var arequipa = data.AQP; // {2016-2: {…}, 2017-1: {…}}
 var mexico = data.CDMX;
 var lima = data.LIM;
 var santiago = data.SCL;
 
+ /* Arreglo de las promociones por sede */
+var promosAQP = Object.keys(arequipa); // ["2016-2", "2017-1"]
+var promosCDMX = Object.keys(mexico); // ["2017-1", "2017-2"]
+var promosLIM = Object.keys(lima); // ["2016-2", "2017-1", "2017-2"]
+var promosSCL = Object.keys(santiago); // ["2016-2", "2017-1", "2017-2"]
+
 /* suma de todas las estudiantes de las 2 promos de Arequipa */
- var allStudentsForSede = (Object.keys(data.AQP['2016-2'].students)).length + (Object.keys(data.AQP['2017-1'].students)).length;
+var suma = (Object.keys(data.AQP['2016-2'].students)).length + (Object.keys(data.AQP['2017-1'].students)).length;
 
 
-add
+
+    // subSelectPromo = document.createElement('div');
+  // imagesContainer.appendChild(subSelectPromo);
+ 
+  // for (var i = 0; i < arrayPromo.length; i++)
+  //   createImage(arrayPromo[i], sede);
+
 /*
  * Funcionalidad de tu producto
  */
